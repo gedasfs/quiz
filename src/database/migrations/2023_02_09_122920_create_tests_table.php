b7f7b627f45e174868c401736b3e0a5c40b0c9c0
gedas.fs <gedas.fs@gmail.com>
2023-02-09 15:04:52 +0200

    Add migrations

--- src/database/migrations/2023_02_09_122920_create_tests_table.php
+++ src/database/migrations/2023_02_09_122920_create_tests_table.php
@@ -13,14 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('tests', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnUpdate()->nullOnDelete();
+            $table->string('name', 255);
+            $table->unsignedInteger('allowed_time');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('tests');
     }
 };
--- src/database/migrations/2023_02_09_125512_create_correct_multiples_table.php
+++ src/database/migrations/2023_02_09_125512_create_correct_multiples_table.php
@@ -13,14 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('correct_multiples', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_admin')->default(false);
-            $table->rememberToken();
+            $table->foreignId('question_multiples_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('answer_multiples_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('correct_multiples');
     }
 };
